94efb1694f75be03f117624ed7d58791932da18d
James Baxter <j.w.baxter@gmail.com>
2015-04-05 00:44:10 +0200

    Make it a http server

--- hello.js
+++ hello.js
@@ -1,1 +1,10 @@
+var http = require('http')
+
+var server = http.createServer(function(request, response) {
+	response.write('<h1>hello world!</h1>');
+	response.write('<h2>What a wonderful world :)</h2>');
+	response.end();
+});
+
+server.listen(8080);
 console.log('hi')
